182bbe4b9aa4eb0922277f38db8fc101e574137c
shariful-alam-2024 <sharifullinkdin2024@gmail.com>
2025-03-27 09:06:19 +0600

    third

---
+++ Chapter-16/Constructor/index.js
@@ -0,0 +1,18 @@
+//practice problem ans:1
+const start = '786';
+const num = +start;
+console.log(num);
+//practice problem ans:2
+const willAttend = 0;
+const data = Boolean(willAttend);
+console.log(data);
+//practice problem ans:3
+const address = new Object();
+address.city = "Manhattan";
+address.country = "US";
+
+console.log(address);
+//practice problem ans:4
+const total = 1979;
+const data1 = total + '';
+console.log(typeof (data1));
