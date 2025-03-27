3e8b0360ace055f662d737c3b536a9eafc58baac
shariful-alam-2024 <sharifullinkdin2024@gmail.com>
2025-03-27 08:05:34 +0600

    second-commit

---
+++ Chapter-15/Scope/index.js
@@ -0,0 +1,30 @@
+//practice problem ans:1
+const taxRate = 15;
+function Income(x) {
+     const tax = (x*taxRate) / 100;
+    console.log(tax);
+    
+};
+
+Income(200);
+//practice problem ans:2
+// function myFun(params) {
+//      let insideSecret = "internal secret hiding place";
+// };
+// console.log(insideSecret);
+// //ReferenceError: insideSecret is not defined
+// //practice problem ans:3
+// if (condition) {
+//      let temperature = 30;
+// };
+// console.log(temperature);
+//practice problem ans:4
+function schoolDetails(params) {
+     schoolName = "ding dong high school";
+     function displaySchoolName(params) {
+         
+          
+     } 
+     console.log(schoolName);
+     
+} schoolDetails();
