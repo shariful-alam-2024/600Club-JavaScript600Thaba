80d7ac236c2943319e73e03717dcbf5fba1cf854
shariful-alam-2024 <sharifullinkdin2024@gmail.com>
2025-03-27 10:20:01 +0600

    date

---
+++ Chapter-16/Date/index.js
@@ -0,0 +1,26 @@
+//practice problem ans:1
+const now = new Date();
+console.log(now);
+//practice problem ans:2
+const data = new Date(2035, 6, 1514, 45, 30);
+
+console.log(data);
+//practice problem ans:3
+const data1 =new Date().getFullYear()
+
+console.log(data1);
+
+//practice problem ans:4
+const data2 = new Date().setFullYear(2040);
+
+console.log(data2);
+
+//practice problem ans:5
+let val = new Date(2029, 2, 16);
+let data4 = val.getDay();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+console.log(days[data4]);
+
+
+
