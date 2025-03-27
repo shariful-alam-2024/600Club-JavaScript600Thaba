b4b5bdcbd6dd3c99e9d42a73579701b45f7940d7
shariful-alam-2024 <sharifullinkdin2024@gmail.com>
2025-03-27 08:17:41 +0600

    secons

--- Chapter-15/Scope/index.js
+++ Chapter-15/Scope/index.js
@@ -1,12 +1,12 @@
 //practice problem ans:1
-const taxRate = 15;
-function Income(x) {
-     const tax = (x*taxRate) / 100;
-    console.log(tax);
+// const taxRate = 15;
+// function Income(x) {
+//      const tax = (x*taxRate) / 100;
+//     console.log(tax);
     
-};
+// };
 
-Income(200);
+// Income(200);
 //practice problem ans:2
 // function myFun(params) {
 //      let insideSecret = "internal secret hiding place";
@@ -19,12 +19,12 @@ Income(200);
 // };
 // console.log(temperature);
 //practice problem ans:4
-function schoolDetails(params) {
-     schoolName = "ding dong high school";
-     function displaySchoolName(params) {
+// function schoolDetails(params) {
+//      schoolName = "ding dong high school";
+//      function displaySchoolName(params) {
          
           
-     } 
-     console.log(schoolName);
+//      } 
+//      console.log(schoolName);
      
-} schoolDetails();
+// } schoolDetails();
