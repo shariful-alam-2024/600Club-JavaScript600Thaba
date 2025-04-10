5c0564a8c6e57bc2884110a0cce8596230c229d5
shariful-alam-2024 <sharifullinkdin2024@gmail.com>
2025-04-10 07:54:44 +0600

    assignment

---
+++ Chapter-20/Assignment-Operator/index.js
@@ -0,0 +1,42 @@
+//practice problem ans:1
+let a = 59;
+let b=a++;
+// let c= ++a;
+console.log(b);
+// console.log(c);
+//practice problem ans:2
+let  orange = 100;
+orange /= 15;
+console.log(orange);
+//practice problem ans:3
+let mango = 20;
+mango &&= 10;
+console.log(mango);
+//practice problem ans:4
+let banana = 50;
+banana *= 4;
+console.log(banana);
+//practice problem ans:5
+let grapes = 19;
+grapes ||= 12;
+console.log(grapes);
+//practice problem ans:6
+let apples = 20;
+apples -= 10;
+console.log(apples);
+//practice problem ans:7
+let price = undefined;
+price ||= 90;
+console.log(price);
+
+//practice problem ans:8
+let tomato = 0;
+tomato &&= 5;
+console.log(tomato);
+//practice problem ans:8
+let apples1 = 15;
+apples1 %= 6;
+console.log(apples1);
+
+
+
