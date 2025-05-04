80806d59b685764d5f10d03054b1c43df35b19a0
shariful-alam-2024 <sharifullinkdin2024@gmail.com>
2025-05-04 11:01:47 +0600

    do-while

---
+++ Chapter-23/do-while/index.js
@@ -0,0 +1,21 @@
+//practice problem ans:1
+let item = 3;
+do {
+     console.log(item);
+     item++;
+     
+} while (item > 5);//output is :3
+
+//practice problem ans:1
+let attempts = 12;
+do {
+     console.log(attempts);
+     attempts++;
+} while (attempts < 10);
+//practice problem ans:3
+let name = "John";
+do {
+     console.log(name);
+     name = 'n' + name;
+     
+} while (name.length<10);
