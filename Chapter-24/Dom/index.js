b10e1f588368f3cf6df3341a635c0a0c423d6ff1
shariful-alam-2024 <sharifullinkdin2024@gmail.com>
2025-05-14 11:29:47 +0600

    dom

---
+++ Chapter-24/Dom/index.js
@@ -0,0 +1,45 @@
+//practice problem answer:1
+/* 
+লোকাল স্টোরেজ:
+
+ব্রাউজারে দীর্ঘ  সময় ধরে ডাটা সংরক্ষনের জন্য  লোকাল স্টোরেজ  ব্যাবহৃত হয় ।
+ব্রাউজার  বন্ধ করে চালু করলে ডাটা মুছে যায় না ।
+লোকাল স্টোরেজ থেকে একটি ডাটা যোগ করা যায় , ডাটা বের করে আনা যায়, স্পেসিফিক ডাটা মুছে ফেলা যায় , সব ডাটা ডিলিট করা যায় ।
+সেকসন স্টোরেজ:
+ব্রাউজার  বন্ধ করে চালু করলে ডাটা মুছে যায় ।
+সেকসন স্টোরেজ থেকে একটি ডাটা যোগ করা যায় , ডাটা বের করে আনা যায়, স্পেসিফিক ডাটা মুছে ফেলা যায় , সব ডাটা ডিলিট করা যায় ।
+
+
+*/
+
+
+//practice problem ans:2
+/* 
+localStorage.setItem('mode', 'dark');
+
+
+console.log(localStorage.getItem('mode')); // Output: 'dark'
+
+
+localStorage.setItem('mode', 'light');
+
+
+console.log(localStorage.getItem('mode')); // Output: 'light'
+ */
+//practice problem ans:3
+
+
+/* // Store the value in sessionStorage
+sessionStorage.setItem('userToken', 'abcd1234');
+
+// Retrieve the value from sessionStorage
+let val = sessionStorage.getItem('userToken');
+
+// Log the value to the console
+console.log(val); */
+//practice problem ans:4
+
+/* localStorage.setItem('userName', 'Alex');
+localStorage.removeItem('userName'); */
+//practice problem ans:৫
+/* history.go(-2); */
