6c594ea1135a3aca39c8b8139ead0b0f1390841b
shariful-alam-2024 <sharifullinkdin2024@gmail.com>
2025-05-16 10:29:02 +0600

    foodpanda

---
+++ Chapter-26/FoodPanda/index.js
@@ -0,0 +1,13 @@
+
+const restaurant = [
+     {id:1,name:"Piza Place",menu:[{name:"pepperoni piza",price:12}]},
+     {id:2,name:"sushi house",menu:[{name:"salmon sushi",price:15}]}
+];
+const order = [];
+const reviews = [];
+const DiscountCode = { "SAVE10": 10, "WELCOME15": 15 };
+
+function getFoodPrice(restaurantName,foodName) {
+     
+}
+
