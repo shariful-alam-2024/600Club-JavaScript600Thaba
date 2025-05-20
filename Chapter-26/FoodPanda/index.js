661b0714732494f0a85f9fd35abad1acbd211943
shariful-alam-2024 <sharifullinkdin2024@gmail.com>
2025-05-20 15:42:47 +0600

    resturant

--- Chapter-26/FoodPanda/index.js
+++ Chapter-26/FoodPanda/index.js
@@ -8,7 +8,14 @@ const reviews = [];
 const DiscountCode = { "SAVE10": 10, "WELCOME15": 15 };
 
 function getFoodPrice(restaurantName, foodName) {
-     
-     
+     const res = restaurant.find(p => p.name === restaurantName)
+     if (!res) {
+          console.log('restaurant not found');
+          
+     };
+     const val = res.menu.find(item => item.name === foodName);
+     const data = val ? val.name : "food item not found";
+     return data;
 }
+console.log(getFoodPrice('sushi house','salmon sushi'));
 
