463e96ca266d10f0f7ceef69a3daf9b1302e2caa
shariful-alam-2024 <sharifullinkdin2024@gmail.com>
2025-05-28 21:00:38 +0600

    discoun t

--- Chapter-26/FoodPanda/index.js
+++ Chapter-26/FoodPanda/index.js
@@ -1,21 +1,50 @@
-
 const restaurant = [
-     {id:1,name:"Piza Place",menu:[{name:"pepperoni piza",price:12}]},
-     {id:2,name:"sushi house",menu:[{name:"salmon sushi",price:15}]}
+     { id: 1, name: "Piza Place", menu: [{ name: "pepperoni piza", price: 12 }] },
+     { id: 2, name: "sushi house", menu: [{ name: "salmon sushi", price: 1500 }] }
 ];
+
 const order = [];
 const reviews = [];
-const DiscountCode = { "SAVE10": 10, "WELCOME15": 15 };
+const DiscountCode = { "SAVE10": 5, "WELCOME15": 15 };
 
 function getFoodPrice(restaurantName, foodName) {
      const res = restaurant.find(p => p.name === restaurantName)
      if (!res) {
           console.log('restaurant not found');
-          
-     };
+          return;
+     }
      const val = res.menu.find(item => item.name === foodName);
      const data = val ? val.name : "food item not found";
      return data;
 }
-console.log(getFoodPrice('sushi house','salmon sushi'));
 
+function placeOrder(restaurantName, foodItem, discountCode, customer) {
+     const foodRestaurant = restaurant.find(r => r.name.toLowerCase() === restaurantName.toLowerCase());
+     if (!foodRestaurant) {
+          console.log('restaurant not found');
+          return;
+     }
+
+     const foodItem1 = foodRestaurant.menu.find(p => p.name.toLowerCase() === foodItem.toLowerCase());
+     if (!foodItem1) {
+          console.log('food not found');
+          return;
+     }
+
+     let dist = foodItem1.price;
+
+     if (discountCode) {
+          const discount = DiscountCode[discountCode];  
+          if (discount) {
+               const discountAmount = (dist * discount) / 100;
+               dist -= discountAmount;
+               console.log(`Discount applied (${discountCode}): ${discount}% off`);
+          } else {
+               console.log('Invalid coupon code');
+          }
+     }
+
+     console.log(`Final price for ${customer}: $${dist.toFixed(2)}`);
+     return dist.toFixed(2);
+}
+console.log(placeOrder('sushi house', 'salmon sushi', 'SAVE10', 'Rahim'));
